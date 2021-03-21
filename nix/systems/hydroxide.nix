7863421f76c76e97a26d04d049f5fc1d607970f2
Thomas Hobson <git@hexf.me>
2021-03-21 14:14:44 +1300

    configure hydroxide

--- nix/systems/hydroxide.nix
+++ nix/systems/hydroxide.nix
@@ -2,4 +2,15 @@
 { config, pkgs, ... }: {
 
     networking.interfaces.ens192.useDHCP = true;
+    networking.hostName = "hydroxide";
+
+    services.hydra = {
+        enable = true;
+        hydraURL = "http://localhost:3000"; # externally visible URL
+        notificationSender = "hydra@localhost"; # e-mail of hydra service
+        # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
+        buildMachinesFiles = [];
+        # you will probably also want, otherwise *everything* will be built from scratch
+        useSubstitutes = true;
+    };
 }
