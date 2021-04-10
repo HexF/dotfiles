94d6941f70f44bcaf4ee384091228e6104e5444e
Thomas Hobson <git@hexf.me>
2021-04-11 01:43:38 +1200

    arls

---
+++ packages/audio-reactive-leds/default.nix
@@ -0,0 +1,23 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.python3Packages.buildPythonApplication rec {
+    pname = "audio-reactive-led-strip";
+    version = "1.0.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "HexF";
+      repo = "audio-reactive-led-strip";
+      rev = "v1.0.0";
+      sha256 = "sha256-hCZH/xOLqjrkIBaCr5MnTvMVlbcnglHlyWjh13iyYo4=";
+    };
+
+    propagatedBuildInputs = with pkgs.python3Packages; [ numpy scipy pyaudio ];
+
+    checkPhase = ''
+    '';
+
+    meta = {
+      description = "Audio Reactive LED Strip service";
+    };
+  }
