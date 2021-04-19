2e80a7249ca55064c89752736b5206b2826d9ea3
Thomas Hobson <git@hexf.me>
2021-04-19 13:09:48 +1200

    vulkan beta nvidia driver

--- systems/snowflake.nix
+++ systems/snowflake.nix
@@ -40,6 +40,6 @@
 
   virtualisation.docker.enable = true;
 
-  hardware.nvidia.package = pkgs.linuxPackages.nvidia_x11_beta;
+  hardware.nvidia.package = pkgs.linuxPackages.nvidia_x11_vulkan_beta;
 }
 
