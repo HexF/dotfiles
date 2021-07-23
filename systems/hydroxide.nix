2a067ad91abe4a05b03a601869494ce794d7b111
Thomas Hobson <git@hexf.me>
2021-07-23 15:21:43 +1200

    nix-serve on hydroxide

--- systems/hydroxide.nix
+++ systems/hydroxide.nix
@@ -14,6 +14,28 @@
         useSubstitutes = true;
     };
 
+    services.nginx = {
+        enable = true;
+        virtualHosts = {
+            "bincache.hexf.me" = {
+                serverAliases = [ "binaryCache" ];
+                locations."/".extraConfig = ''
+                    proxy_pass http://localhost:${toString config.services.nix-serve.port};
+                    proxy_set_header Host $host;
+                    proxy_set_header X-Real-IP $remote_addr;
+                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                '';
+            };
+        };  
+
+        
+    };
+
+
+    services.nix-serve = {
+        enable = true;
+    };
+
     nix.buildMachines = [
         {
             hostName = "localhost";
