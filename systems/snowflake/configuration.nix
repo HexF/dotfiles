49f9d02d3699ae640efb83a52d984ad3eee77035
Thomas Hobson <git@hexf.me>
2021-12-18 23:07:44 +1300

    fix

--- systems/snowflake/configuration.nix
+++ systems/snowflake/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   mkBackup = import ../helpers/backup.nix {inherit config;};
 in {
