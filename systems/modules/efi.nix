fa1314014716dc0bb798edeac4d99f118a770673
Thomas Hobson <git@hexf.me>
2021-12-23 17:39:09 +1300

    ensure max resolution in EFI

--- systems/modules/efi.nix
+++ systems/modules/efi.nix
@@ -4,6 +4,7 @@
   
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
   
 }
