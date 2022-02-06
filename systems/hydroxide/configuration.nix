145c244f70093d8bbeb51e8ff101715958e603be
Thomas Hobson <thomas@hexf.me>
2022-02-06 20:31:28 +1300

    Gitea on Hydroxide

--- systems/hydroxide/configuration.nix
+++ systems/hydroxide/configuration.nix
@@ -1,4 +1,3 @@
-# Server for hydra
 { config, pkgs, ... }: {
 
     imports = [
@@ -15,56 +14,18 @@
     networking.interfaces.ens192.useDHCP = true;
     networking.hostName = "hydroxide";
 
-    services.hydra = {
-        enable = true;
-        hydraURL = "https://hydra.hexf.me"; # externally visible URL
-        notificationSender = "hydra@localhost"; # e-mail of hydra service
-        # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
-        buildMachinesFiles = [];
-        # you will probably also want, otherwise *everything* will be built from scratch
-        useSubstitutes = true;
-        extraConfig = ''
-            binary_cache_public_uri = https://binarycache.hexf.me
-        '';
-    };
 
     services.nginx = {
         enable = true;
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
         virtualHosts = {
-            "bincache.hexf.me" = {
-                serverAliases = [ "binaryCache" ];
-                locations."/".extraConfig = ''
-                    proxy_pass http://localhost:${toString config.services.nix-serve.port};
-                    proxy_set_header Host $host;
-                    proxy_set_header X-Real-IP $remote_addr;
-                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                '';
+            "git.hexdev.nz" = {
+                locations."/".proxyPass = "http://localhost:8092/";   # Proxy Gitea
             };
-        };  
-
-        
+        };    
     };
-
-
-    services.nix-serve = {
-        enable = true;
-        secretKeyFile = "/var/cache-priv-key.pem";
-        # Generate with
-        # $ nix-store --generate-binary-cache-key binarycache.example.com cache-priv-key.pem cache-pub-key.pem
-        # # mv cache-priv-key.pem /var/cache-priv-key.pem
-        # # chown nix-serve /var/cache-priv-key.pem
-        # # chmod 600 /var/cache-priv-key.pem
-    };
-
-    nix.buildMachines = [
-        {
-            hostName = "localhost";
-            system = "x86_64-linux";
-            supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
-            maxJobs = 8;
-        }
-    ];
-
 
     services.media-server = {
         enable = true;
@@ -78,8 +39,35 @@
         vhostSuffix = ".oh.hexf.me";
     };
 
+    services.gitea = {
+        enable = true;
+        appName = "HexDev: Gitea";
+        database = {
+            type = "postgres";
+            password = "postgres gitea password";
+        };
+        domain = "git.hexdev.nz";
+        rootUrl = "https://git.hexdev.nz";
+        httpPort = 8092;
+    };
+
+    services.postgresql = {
+        enable = true;
+        authentication = ''
+            local gitea all ident map=gitea-users
+        '';
+        identMap = ''
+            gitea-users gitea gitea
+        '';
+    };
+
     fileSystems."/mnt/media" = {
         device = "192.168.1.101:/volume2/Media";
+        fsType = "nfs";
+    };
+
+    fileSystems."/mnt/src" = {
+        device = "192.168.1.101:/volume2/Thomas Source Code";
         fsType = "nfs";
     };
 
--- systems/hydroxide/modules/media-server.nix
+++ systems/hydroxide/modules/media-server.nix
@@ -66,9 +66,6 @@ in {
                     default = false;
                 };
             };
-
-
-
         };
     };
 
