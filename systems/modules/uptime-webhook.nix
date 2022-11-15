36ef58ae073183cc6681c78c86a3616451c101ba
Thomas Hobson <git@hexf.me>
2022-11-15 13:13:29 +1300

    update time format in webhook

--- systems/modules/uptime-webhook.nix
+++ systems/modules/uptime-webhook.nix
@@ -14,7 +14,7 @@
       serviceConfig.Type = "oneshot";
       path = with pkgs; [bash curl gawk];
       script = ''
-        awk '{print "{\"content\":\"HexF Uptime: "int($1/86400)":"int(($1%86400)/3600)":"int(($1%3600)/60)":"int($1%60)"\"}"}' /proc/uptime | curl -XPOST -H "Content-Type: application/json" --data @- $(cat ${config.sops.secrets.snowflake_uptime_webhook.path})
+        awk '{print "{\"content\":\"HexF Uptime: "int($1/86400)" days, "int(($1%86400)/3600)" hours, "int(($1%3600)/60)" minutes, "int($1%60)" seconds\"}"}' /proc/uptime | curl -XPOST -H "Content-Type: application/json" --data @- $(cat ${config.sops.secrets.snowflake_uptime_webhook.path})
       '';
   };
 
