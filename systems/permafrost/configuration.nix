bad165322b6ee03de359d15b2db4c057d06f023d
Thomas Hobson <thomas@hexf.me>
2023-08-28 02:05:58 +1200

    set hostname to tailnet name

--- systems/permafrost/configuration.nix
+++ systems/permafrost/configuration.nix
@@ -33,12 +33,12 @@ in {
     services.nextcloud = {
         enable = true;
         package = pkgs.nextcloud27;
-        hostName = "nextcloud.localhost";
+        hostName = "nextcloud.${tailnet}";
         config = {
             adminpassFile = "${pkgs.writeText "adminpass" "test123"}"; #immediately change this lol
             dbtype = "pgsql";
             extraTrustedDomains = [
-                "nextcloud.${tailnet}"
+                
             ];
         };
 
