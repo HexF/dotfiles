c3756f43a3131e1d40d83d10d024a59036f6415c
Thomas Hobson <thomas@hexf.me>
2024-03-03 20:46:17 +1300

    vr vm

--- systems/permafrost/configuration.nix
+++ systems/permafrost/configuration.nix
@@ -320,7 +320,7 @@ in {
         "vfio_pci"
         "vfio"
         "vfio_iommu_type1"
-        "vfio_virqfd"
+#        "vfio_virqfd"
       ];
 
     virtualisation.spiceUSBRedirection.enable = true;
