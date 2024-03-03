d8f06bbfd4a0c69b3b19adb6f2e2ca327578afc4
Thomas Hobson <thomas@hexf.me>
2024-03-03 19:52:13 +1300

    plasma5

--- systems/permafrost/configuration.nix
+++ systems/permafrost/configuration.nix
@@ -303,6 +303,7 @@ in {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
 
     users.users.htpc = {
         isNormalUser  = true;
