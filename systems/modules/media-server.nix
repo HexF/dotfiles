8ec3b7b599c3073c6aeb18122f6297e0dafff54c
Thomas Hobson <thomas@hexf.me>
2026-06-21 10:48:27 +1000

    remove rclone syncing

--- systems/modules/media-server.nix
+++ systems/modules/media-server.nix
@@ -315,24 +315,24 @@ in {
 
 
         # rclone sync with seedbox
-        systemd.services.rclone-seedbox-sync = {
-            description = "Sync torrent files to seedbox using rclone";
-            serviceConfig = {
-                Type = "oneshot";
-                User = cfg.user;
-                ExecStart = "${pkgs.rclone}/bin/rclone --config /var/lib/media/rclone.conf sync 'ftp1:/torrents' /var/lib/media/remote/torrents/ -v -P --transfers 8";
-            };
-        };
+        # systemd.services.rclone-seedbox-sync = {
+        #     description = "Sync torrent files to seedbox using rclone";
+        #     serviceConfig = {
+        #         Type = "oneshot";
+        #         User = cfg.user;
+        #         ExecStart = "${pkgs.rclone}/bin/rclone --config /var/lib/media/rclone.conf sync 'ftp1:/torrents' /var/lib/media/remote/torrents/ -v -P --transfers 8";
+        #     };
+        # };
 
-        systemd.timers.rclone-seedbox-sync = {
-            description = "Run Rclone sync every 5 minutes";
-            wantedBy = [ "timers.target" ];
-            timerConfig = {
-                OnBootSec = "5m";
-                OnUnitActiveSec = "5m";
-                Unit = "rclone-seedbox-sync.service";
-            };
-        };
+        # systemd.timers.rclone-seedbox-sync = {
+        #     description = "Run Rclone sync every 5 minutes";
+        #     wantedBy = [ "timers.target" ];
+        #     timerConfig = {
+        #         OnBootSec = "5m";
+        #         OnUnitActiveSec = "5m";
+        #         Unit = "rclone-seedbox-sync.service";
+        #     };
+        # };
 
 
         # qui - qBitTorrent web ui
